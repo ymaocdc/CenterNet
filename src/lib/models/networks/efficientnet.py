f7dbd3ce6f4a139aef150b272e10d99dbf8c236a
YuShen <yus@xsense.ai>
2020-01-06 15:35:00 -0800

    Add efficient net b0 ~ b7

---
+++ src/lib/models/networks/efficientnet.py
@@ -0,0 +1,115 @@
+import torch.nn as nn
+from .resnet_dcn import fill_fc_weights, fill_up_weights
+from .DCNv2.dcn_v2 import DCN
+from efficientnet_pytorch import EfficientNet
+
+BN_MOMENTUM = 0.1
+
+
+class EffNet(nn.Module):
+
+    def __init__(self, version, heads, head_conv, inplanes=1280):
+        super(EffNet, self).__init__()
+        self.inplanes = inplanes
+        self.deconv_with_bias = False
+        self.heads = heads
+        self.head_conv = head_conv
+        self.model = EfficientNet.from_pretrained('efficientnet-b' + str(version))
+
+        # used for deconv layers
+        self.deconv_layers = self._make_deconv_layer(
+            3,
+            [256, 128, 64],
+            [4, 4, 4],
+        )
+
+        for head in self.heads:
+            classes = self.heads[head]
+            if head_conv > 0:
+                fc = nn.Sequential(
+                    nn.Conv2d(self.inplanes, head_conv,
+                              kernel_size=3, padding=1, bias=True),
+                    nn.ReLU(inplace=True),
+                    nn.Conv2d(head_conv, classes,
+                              kernel_size=1, stride=1,
+                              padding=0, bias=True))
+                if 'hm' in head:
+                    fc[-1].bias.data.fill_(-2.19)
+                else:
+                    fill_fc_weights(fc)
+            else:
+                fc = nn.Conv2d(self.inplanes, classes,
+                               kernel_size=1, stride=1,
+                               padding=0, bias=True)
+                if 'hm' in head:
+                    fc.bias.data.fill_(-2.19)
+                else:
+                    fill_fc_weights(fc)
+            self.__setattr__(head, fc)
+
+    def _get_deconv_cfg(self, deconv_kernel, index):
+        if deconv_kernel == 4:
+            padding = 1
+            output_padding = 0
+        elif deconv_kernel == 3:
+            padding = 1
+            output_padding = 1
+        elif deconv_kernel == 2:
+            padding = 0
+            output_padding = 0
+
+        return deconv_kernel, padding, output_padding
+
+    def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
+        assert num_layers == len(num_filters), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+        assert num_layers == len(num_kernels), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+
+        layers = []
+        for i in range(num_layers):
+            kernel, padding, output_padding = \
+                self._get_deconv_cfg(num_kernels[i], i)
+
+            planes = num_filters[i]
+            fc = DCN(self.inplanes, planes,
+                     kernel_size=(3, 3), stride=1,
+                     padding=1, dilation=1, deformable_groups=1)
+            # fc = nn.Conv2d(self.inplanes, planes,
+            #         kernel_size=3, stride=1,
+            #         padding=1, dilation=1, bias=False)
+            # fill_fc_weights(fc)
+            up = nn.ConvTranspose2d(
+                in_channels=planes,
+                out_channels=planes,
+                kernel_size=kernel,
+                stride=2,
+                padding=padding,
+                output_padding=output_padding,
+                bias=self.deconv_with_bias)
+            fill_up_weights(up)
+
+            layers.append(fc)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(up)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            self.inplanes = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        features = self.model.extract_features(x)
+        features = self.deconv_layers(features)
+        ret = {}
+        for head in self.heads:
+            ret[head] = self.__getattr__(head)(features)
+        return [ret]
+
+
+def get_eff_net(num_layers, heads, head_conv):
+    # NOTE: num_layers in efficientnet means b0 ~ b7 model.
+    print('efficientnet-b' + str(num_layers))
+    model = EffNet(num_layers, heads, head_conv)
+    return model
--- src/lib/opts.py
+++ src/lib/opts.py
@@ -61,7 +61,7 @@ class opts(object):
     self.parser.add_argument('--arch', default='dla_34', 
                              help='model architecture. Currently tested'
                                   'res_18 | res_101 | resdcn_18 | resdcn_101 |'
-                                  'dlav0_34 | dla_34 | hourglass')
+                                  'dlav0_34 | dla_34 | hourglass | efficientnet_0~7')
     self.parser.add_argument('--head_conv', type=int, default=-1,
                              help='conv layer channels for output head'
                                   '0 for no conv layer'
