l1 loss roll back and fix quaternion preprocessing

src/lib/datasets/sample/pkudet.py
@@ -290,18 +290,18 @@ class PKUDataset(data.Dataset):
                 yaw, pitch, roll = ann['global_yaw'], ann['pitch'], ann['roll']
                 if yaw > np.pi:
                     yaw = yaw - np.pi * 2
-                pitch, yaw, roll = -yaw, -pitch, -roll
+                # pitch, yaw, roll = -yaw, -pitch, -roll
                 qn = self.euler_angles_to_quaternions(np.array([yaw, pitch, roll]))[0]
                 q = self.quaternion_upper_hemispher(qn)
-                # if (q-qn).sum() > 0:
-                #     print( '>0:   ', ann['global_yaw'], ann['pitch'], ann['roll'])
-                # else:
-                #     print('=0:   ',ann['global_yaw'], ann['pitch'], ann['roll'])
+                from utils.post_process import quaternion_to_euler_angle
+                # norm = np.linalg.norm(q, axis=0)
+                # q = q / norm
+                euler_angle = quaternion_to_euler_angle(q)
+                if np.abs((euler_angle - [yaw, pitch, roll])).sum()>0.02:
+                    print(euler_angle - [yaw, pitch, roll])
                 if self.opt.reg_q:
                     reg_q[k] = q
                     reg_q_mask[k] = 1
-                    if flipped:
-                        reg_q_mask[k] = 0
                     gt_det[-1] = gt_det[-1] + q.tolist()
 
                 dep[k] = ann['3D_location'][2]

src/lib/models/losses.py
@@ -181,8 +181,8 @@ class L1Loss(nn.Module):
   def forward(self, output, mask, ind, target):
     pred = _transpose_and_gather_feat(output, ind)
     mask = mask.unsqueeze(2).expand_as(pred).float()
-    # loss = F.l1_loss(pred * mask, target * mask, reduction='mean')
-    loss = (pred * mask - target * mask).sum() / mask.sum()
+    loss = F.l1_loss(pred * mask, target * mask, reduction='mean')
+    # loss = (pred * mask - target * mask).abs().sum() / mask.sum()
     return loss
 
 class BinRotLoss(nn.Module):

src/lib/trains/pku.py
@@ -42,7 +42,7 @@ class DddLoss(torch.nn.Module):
 
             hm_loss += self.crit(output['hm'], batch['hm']) / opt.num_stacks
             if opt.dep_weight >= 0:
-                dep_loss += self.crit_reg(output['dep'], batch['reg_mask'],
+                dep_loss += self.crit_reg(output['dep'], batch['reg_q_mask'],
                                           batch['ind'], batch['dep']) / opt.num_stacks
             if opt.dim_weight >= 0:
                 dim_loss += self.crit_reg(output['dim'], batch['reg_mask'],